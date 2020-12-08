109cec1689fde30af3ded44ea585c1ff8e4e1279
anton bendryk <antonbendrik1993@gmail.com>
2020-12-08 18:34:09 +0200

    first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import "./App.sass";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      npm 
+      </header>
+    </div>
+  );
+}
+
+export default App;
