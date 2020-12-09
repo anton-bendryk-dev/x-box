1162ea054528bfca2e3a5f54b602bc7000327b60
anton bendryk <antonbendrik1993@gmail.com>
2020-12-09 16:54:39 +0200

    complete header

--- src/App/header/header.jsx
+++ src/App/header/header.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>header</header>
+        <header>
+            <div className="logo-container">
+                <a href='/' className="logo"></a>
+            </div>
+            <div className="menu-container">
+                <div className="menu-button">
+                    <a href='/'>Games<span className="arrow"></span></a>
+                </div>
+                <div className="menu-button">
+                    <a href='/'>Consoles<span className="arrow"></span></a>
+                </div>
+                <div className="menu-button">
+                    <a href='/'>Community</a>
+                </div>
+            </div>
+            <div className="search-container">
+                <div className="search">
+                    <input type="text" placeholder="Search"></input>
+                </div>
+            </div>
+            <div className="user-container">
+                <span className="user-icon"></span>
+                <a href="/">My XBOX<span className="arrow"></span></a>
+            </div>
+        </header>
     )
 }
 
