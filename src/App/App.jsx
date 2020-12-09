0152b4da2634fab78ab276d3a420aa278f07d97c
anton bendryk <antonbendrik1993@gmail.com>
2020-12-09 13:43:58 +0200

    added components header, main and footer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,16 @@
+import React from 'react'
 import "./style/App.sass";
+import Header from './header/header';
+import Main from './main/main';
+import Footer from './footer/footer';
 
 function App() {
   return (
     <div className="wrapper">
       <div className="inline-wrapper">
-        <header>header</header>
-        <main>
-          <section>main</section>
-          <section>main</section>
-          <section>main</section>
-          <section>main</section>
-        </main>
-        <footer>footer</footer>
+        <Header/>
+        <Main/>
+        <Footer/>
       </div>
     </div>
   );
---
+++ src/App/main/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+            <section>main</section>
+            <section>main</section>
+            <section>main</section>
+            <section>main</section>
+        </main>
+    )
+}
+
+export default Main
